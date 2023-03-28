e5888594d0e2654f71aeaae55a37ab496489d14c
Dmitry <128856836+DmitryIordanov@users.noreply.github.com>
2023-03-28 19:21:09 +0300

    Php-blog-project
    finalization of the output of articles and the implementation of additional functionality

--- index.php
+++ index.php
@@ -4,24 +4,59 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
+use Blog\PostMapper;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $loader = new FilesystemLoader('templates');
-
 $view = new Environment($loader);
+
+$config = include 'config/database.php';
+$dns = $config['dsn'];
+$username = $config['username'];    
+$password = $config['password'];
+
+try {
+    $connection = new PDO($dns, $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch(PDOException $extension){
+    echo 'Database error: ' . $extension->getMessage();
+    exit;
+}
+
+$PostMapper = new PostMapper($connection);
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) use ($view) {
-    $body = $view->render("index.twig");
+$app->get('/', function (Request $request, Response $response, $args) use ($view, $PostMapper) {
+    $posts = $PostMapper->getList('DESC');
+
+    $body = $view->render("index.twig", [
+        'posts' => $posts
+    ]);
     $response->getBody()->write($body);
     return $response;
 });
+
 $app->get('/about', function (Request $request, Response $response, $args) use ($view) {
     $body = $view->render("about.twig", [
         'name' => 'Dimon'
     ]);
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $PostMapper) {
+    $post = $PostMapper->getByUrlKey((string) $args['url_key']);
+
+    if (empty($post)) {
+        $body = $view->render("404.twig");
+    } else {
+        $body = $view->render("post.twig", [
+            'post' => $post
+        ]);
+    }
     $response->getBody()->write($body);
     return $response;
 });
